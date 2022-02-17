d7ca20a67731736f71ac13e211043629a371f3a5
Tom <94thartman@gmail.com>
2022-02-16 20:51:28 -0500

    simplified menu button

--- components/MenuButton.js
+++ components/MenuButton.js
@@ -1,19 +1,14 @@
 import React from 'react'
-import tw, { styled } from 'twin.macro'
+import tw from 'twin.macro'
 import { faBars } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-const Button = styled.button`
-  ${tw`bg-opacity-20 hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 relative inline-flex items-center justify-center h-10 gap-2 px-4 text-white bg-blue-500 rounded-md`}
-`
+const Button = tw.button`hover:opacity-100 opacity-80 text-3xl text-white transition-all duration-300 ease-in-out`
 
 const MenuButton = ({ ...rest }) => {
   return (
     <Button {...rest}>
       <FontAwesomeIcon icon={faBars} />
-      <div tw="h-full flex flex-col items-center justify-center">
-        <p tw="leading-none">menu</p>
-      </div>
     </Button>
   )
 }
