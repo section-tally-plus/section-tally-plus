1a58104cc89e9e66958e44752da0bd599f0b31f9
zmanwinner <zleesman@gmail.com>
2022-03-04 09:50:59 -0500

    added something

--- pages/api/courses.js
+++ pages/api/courses.js
@@ -2,6 +2,7 @@ import { connectToDatabase } from '../../util/mongodb'
 
 export default async function handler(req, res) {
   const { db } = await connectToDatabase()
-  const allCourses = await db.collection('stp_202230').find({}).toArray()
+  const query = req.query
+  const allCourses = await db.collection(query.semester).find({}).toArray()
   res.status(200).json(JSON.stringify(allCourses, null, 2))
 }
