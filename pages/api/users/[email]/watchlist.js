52820f73b96fdc73a6726459dedb6413d5c3e4bc
zmanwinner <zleesman@gmail.com>
2022-03-25 21:17:55 -0400

    update immediately (definitely not correctly)

--- pages/api/users/[email]/watchlist.js
+++ pages/api/users/[email]/watchlist.js
@@ -2,13 +2,10 @@ import { connectToDatabase } from '../../../../util/mongodb'
 export default async function handler(req, res) {
   const { db } = await connectToDatabase()
   if (req.method == 'GET') {
-    if (req.method == 'GET') {
-      const result = await db
-        .collection('users')
-        .find({ email: { $in: [req.query.email] } })
-        .toArray()
-      res.status(200).json(JSON.stringify(result.watchlist, null, 2))
-    }
+    const result = await db
+      .collection('users')
+      .findOne({ email: { $in: [req.query.email] } })
+    res.status(200).json(JSON.stringify(result.watchlist, null, 2))
   }
   if (req.method == 'POST') {
     const courseInfo = req.body
